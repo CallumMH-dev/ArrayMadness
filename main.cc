eb1eb6ad05bb87c1497861c79b2e894b4d9e16fd
Callum <174373194+CallumMH-dev@users.noreply.github.com>
2025-11-04 00:42:47 +0000

    refactoring

--- main.cc
+++ main.cc
@@ -3,14 +3,13 @@ const unsigned HEIGHT = 20;
 const unsigned WIDTH  = 20;
 int array[HEIGHT*WIDTH];
 
-void INIT_ARRAY(int* array){ for(unsigned i {0}; i < HEIGHT*WIDTH; ++i){ array[i] = 0; } }
+void INIT_ARRAY(int* array, unsigned i = 0){ for(;i < HEIGHT*WIDTH;){ array[i++] = 0; } }
 
-void PRINT_ARRAY(int* array, unsigned counter = 0)
+void PRINT_ARRAY(int* array, unsigned counter = 0, unsigned i = 0)
 {
- for(unsigned i {0}; i < HEIGHT*WIDTH; ++i)
+ for(;i < HEIGHT*WIDTH;)
  {
-  if( array[i] == 0 )           { std::cout << '.'; }
-  else                          { std::cout << '#'; }
+  if( array[i++] == 0 ){ std::cout << '.'; } else { std::cout << '#'; }
   if( counter++ == (WIDTH - 1) ){ counter = 0; std::cout << '\n';}
  }
 }
@@ -19,8 +18,7 @@ bool CHANGE_ELEMENT_VIA_COORDINATES(int* array, unsigned x = 0, unsigned y = 0)
 {
  std::cout << "please enter an X followed by a Y coordinate" << std::endl;
  std::cin >> std::ws >> x >> y;
- if( x >= WIDTH || y >= HEIGHT ) return 1;
- array[y * WIDTH + x] = 1;
+ if( x >= WIDTH || y >= HEIGHT ){ return 1; } else { array[y * WIDTH + x] = 1; }
  return 0;
 }
 int main()
