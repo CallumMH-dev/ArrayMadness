c6ef2fd90947337f1b23d1388c1761e819892487
Callum <174373194+CallumMH-dev@users.noreply.github.com>
2025-11-04 01:07:52 +0000

    Added note for future self:
    Incase I come back and have no idea what I'm looking at.

--- main.cc
+++ main.cc
@@ -1,3 +1,4 @@
+/* Printing and editing pseudo 2D array by Callum Hartley */
 #include <iostream>
 
 const unsigned HEIGHT = 20;
@@ -31,3 +32,13 @@ int main()
  else{ std::cout << "invalid coord, array is " << HEIGHT << " * " << WIDTH << std::endl; }
  return 0;
 }
+
+/* NOTES: Thought it would be a good idea to leave a comment for future self incase I ever look over this and get confused.
+   The premise behind y * WIDTH + x is relatively simple, first to clear up any confusion HEIGHT is Y is rows and WIDTH is X is cols, in terms of a 2D array( array[HEIGHT][WIDTH] )
+   now the formula of y * WIDTH + x can be better explained broken down, first we have y * WIDTH = how many rows down we want to go, i.e. 0*20 = first row, 1 * 20 = second row etc.
+
+   Now if you're lost just remember WIDTH is the cols part of an array, we can assume every row has 19 elements( index 0 ), so when it ticks over we will be at the first element of the
+   next row( array[1][0] == 1 * 20 ).
+
+   Next we have + x which is going to traverse the current cols on the row of n * WIDTH.
+*/
